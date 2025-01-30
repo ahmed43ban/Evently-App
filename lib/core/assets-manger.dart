0eeb3384a315caf1e71fe3276256c693a8d2aab3
Ahme43ban <shaabanahmed531@gmail.com>
2025-01-30 10:32:35 +0200

    added assets-manger.dart

--- lib/core/assets-manger.dart
+++ lib/core/assets-manger.dart
@@ -1,10 +1,19 @@
 abstract class AssetsManger{
   static const String logo = "assets/images/logo.png";
   static const String beingCreative = "assets/images/being-creative.png";
+  static const String beingCreativeDark = "assets/images/beingCreativeDark.png";
   static const String beingCreative2 = "assets/images/being-creative 2.png";
+  static const String beingCreativeDark2 = "assets/images/beingCreativeDark2.png";
+  static const String beingCreativeDark3 = "assets/images/beingCreativeDark3.png";
   static const String beingCreative3 = "assets/images/being-creative 3.png";
   static const String arabicLan = "assets/images/EG.svg";
   static const String englishLan = "assets/images/LR.svg";
   static const String moonIcon = "assets/images/Moon.svg";
   static const String sunIcon = "assets/images/Sun.svg";
+  static const String passwordIcon = "assets/images/password.svg";
+  static const String changeSetting = "assets/images/change-setting.svg";
+  static const String googleIcon = "assets/images/google_icon.svg";
+  static const String personIcon = "assets/images/person.svg";
+  static const String emailIcon = "assets/images/email.svg";
+  static const String logo2 = "assets/images/logo2.png";
 }
